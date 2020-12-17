5fbd76990d89910f146f7deb9e526cbe429955a8
dawihailu2406 <dawit.hailu2406@gmail.com>
2020-12-17 03:31:49 -0600

    same changes

--- ExserciseApplication/src/main/java/edu/miu/student/repository/StudentRepository.java
+++ ExserciseApplication/src/main/java/edu/miu/student/repository/StudentRepository.java
@@ -7,4 +7,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository  extends JpaRepository<Student,Long> {
+//    @Query("select p from Product p where p.productName like %:searchString% and p.release='Yes'")
+//    Page<Product> findByProductName(Pageable var1,String searchString);
+//
+//    //      @Query(value="select p from Product p where p.id=:categoryId")
+////      List<Product> findAllByCategoryId(long categoryId);
+//    List<Product> findAllByOrderByUpLoadedDate();
+//
+//    @Query("select p.user from Product p where p.id=: pId")
+//    User findUserByProductId(Long pId);
+//}
 }
